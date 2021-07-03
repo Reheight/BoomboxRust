88081eaa1e649bdc23549ecd0b7ffe2df0bc1fe0
Dustin Palmatier <dustinpalmatier@icloud.com>
2021-07-02 23:44:50 -0400

    Update BoomboxURL.cs

--- BoomboxURL.cs
+++ BoomboxURL.cs
@@ -1,18 +1,28 @@
 using Newtonsoft.Json;
 using Oxide.Core.Libraries.Covalence;
-using System.Linq;
 using UnityEngine;
 using System.Reflection;
+using System.Collections.Generic;
+using System;
+using Oxide.Core;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace Oxide.Plugins
 {
-    [Info("BoomboxURL", "Reheight", "1.0.0")]
+    [Info("BoomboxURL", "Reheight/RyanJD", "1.0.0")]
     [Description("Allows players to easily set the url of a boombox.")]
     public class BoomboxURL : RustPlugin
     {
         private const string UsePerm = "boomboxurl.use";
 
         private PropertyInfo _serverIpinfo = typeof(BoomBox).GetProperty("CurrentRadioIp");
+        Settings config;
+
+        Regex limitedURLS;
+        string presetStationsList;
+
+        Dictionary<int, string> stationsNumbered = new Dictionary<int, string>();
 
         private void SetBoomBoxServerIp(BoomBox box, string ip)
         {
@@ -27,38 +37,135 @@ namespace Oxide.Plugins
         {
             permission.RegisterPermission(UsePerm, this);
             AddCovalenceCommand("boombox", nameof(boomboxCMD));
+            AddCovalenceCommand("stations", nameof(stationsCMD));
+            AddCovalenceCommand("station", nameof(stationCMD));
+        }
+
+        private class Settings
+        {
+            [JsonProperty(PropertyName = "Whitelist")]
+            public bool Whitelist = true;
+
+            [JsonProperty(PropertyName = "Whitelisted Domains")]
+            public List<string> WhitelistedDomains = new List<string>()
+            {
+                "stream.zeno.fm"
+            };
+
+            [JsonProperty(PropertyName = "Preset Stations")]
+            public Dictionary<string, string> PresetStations = new Dictionary<string, string>()
+            {
+                { "Country Hits", "http://crystalout.surfernetwork.com:8001/KXBZ_MP3" },
+                { "Todays Hits", "https://rfcmedia.streamguys1.com/MusicPulsePremium.mp3"},
+                { "Pop Hits", "https://rfcmedia.streamguys1.com/newpophitspremium.mp3" }
+            };
+        }
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+
+            try
+            {
+                config = Config.ReadObject<Settings>();
+                if (config == null) throw new Exception();
+            }
+            catch
+            {
+                Config.WriteObject(config, false, $"{Interface.Oxide.ConfigDirectory}/{Name}.jsonError");
+                PrintError("The configuration for Discord Authentication seems to contain and error and was replaced with the default configuration, your previous configuration has been renamed with a .jsonError extension!");
+                LoadDefaultConfig();
+            }
+
+            limitedURLS = new Regex($@"^(http||https):\/\/({ String.Join("|", config.WhitelistedDomains) }).*", RegexOptions.Compiled);
+            StringBuilder stationsBuilder = new StringBuilder("The following stations we have are below!\n\n");
+
+            int index = 1;
+            foreach (var item in config.PresetStations)
+            {
+                stationsBuilder.Append(index.ToString()).Append(". ").AppendLine(item.Key);
+
+                stationsNumbered[index] = item.Value;
+                index++;
+            }
+
+            stationsBuilder.AppendLine().Append("Type /station <number> to play a station while holding or looking at a boombox!");
+
+            presetStationsList = stationsBuilder.ToString();
+        }
+        protected override void LoadDefaultConfig() => config = new Settings();
+
+        protected override void SaveConfig() => Config.WriteObject(config);
+
+        private void stationsCMD(IPlayer player, string cmd, string[] args)
+        {
+            player.Reply(presetStationsList);
+        }
+
+        private void stationCMD(IPlayer player, string cmd, string[] args)
+        {
+            int index;
+            string stationURL;
+
+            if (!int.TryParse(args[0], out index) || !stationsNumbered.TryGetValue(index, out stationURL))
+            {
+                player.Reply("You must input a number that correlated to a station!");
+                return;
+            }
+
+            switchStation(player.Object as BasePlayer, stationURL);
         }
 
         private void boomboxCMD(IPlayer player, string cmd, string[] args)
         {
             if (!permission.UserHasPermission(player.Id, UsePerm)  && !player.IsAdmin)
+            {
                 player.Reply("You do not have permission to use this command!");
+                return;
+            }
 
             if (args.Length <= 0)
+            {
                 player.Reply("You must provide a URL to a audio stream!");
+                return;
+            }
 
-            BasePlayer plr = player?.Object as BasePlayer;
+            if (!args[0].StartsWith("http"))
+                args[0] = $"https://{args[0]}";
 
-            Item heldItem = plr.GetActiveItem();
+            if (config.Whitelist && !limitedURLS.IsMatch(args[0]))
+            {
+                player.Reply("You must use an accepted URL/Domain");
+                return;
+            }
+
+            switchStation(player.Object as BasePlayer, args[0]);
+        }
+
+        private void switchStation(BasePlayer player, string station)
+        {
+
+            Item heldItem = player.GetActiveItem();
 
             if (heldItem == null || heldItem.info.shortname != "fun.boomboxportable")
             {
                 DeployableBoomBox boombox;
-                if (!IsLookingAtBoomBox(plr, out boombox))
+                if (!IsLookingAtBoomBox(player, out boombox))
                 {
-                    plr.ChatMessage("You must be holding or looking at a boombox!");
+                    player.ChatMessage("You must be holding or looking at a boombox!");
                     return;
                 }
 
                 boombox.BoxController.ServerTogglePlay(false);
-                SetBoomBoxServerIp(boombox, args[0]);
+                SetBoomBoxServerIp(boombox, station);
                 boombox.BoxController.ServerTogglePlay(true);
-            } else
+            }
+            else
             {
                 BoomBox heldBoombox = heldItem.GetHeldEntity().GetComponent<BoomBox>();
 
                 heldBoombox.ServerTogglePlay(false);
-                SetBoomBoxServerIp(heldBoombox, args[0]);
+                SetBoomBoxServerIp(heldBoombox, station);
                 heldBoombox.ServerTogglePlay(true);
             }
         }
